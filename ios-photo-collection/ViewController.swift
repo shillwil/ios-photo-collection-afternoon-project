1663eddc2a56fd11224a544ca0a370e905122ef8
shillwil <alex.shillingford@icloud.com>
2019-06-13 13:34:42 -0600

    First commit

---
+++ ios-photo-collection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-photo-collection
+//
+//  Created by Alex Shillingford on 6/13/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
